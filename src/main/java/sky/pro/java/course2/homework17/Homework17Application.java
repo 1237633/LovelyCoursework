a6c5dd724542bc9f9c5c6f8378a5feb43199c746
ВасянПупкин <tgoroshek@gmail.com>
2023-05-10 21:46:27 +0300

    init

---
+++ src/main/java/sky/pro/java/course2/homework17/Homework17Application.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.homework17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework17Application.class, args);
+    }
+
+}
