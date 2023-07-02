d0a3457ce9c9b2c19e4263d41ac0861614cb4a01
ВасянПупкин <tgoroshek@gmail.com>
2023-07-03 00:00:23 +0300

    Initial commit

---
+++ src/test/java/sky/pro/java/course2/homework17/Homework17ApplicationTests.java
@@ -0,0 +1,15 @@
+package sky.pro.java.course2.homework17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootTest
+class Homework17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
